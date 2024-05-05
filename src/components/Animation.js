7b57a666357dfd6140da5a6e0a00619c3106147c
sachit <sachitdahal33@gmail.com>
2024-05-05 12:30:13 +0545

    new gif added

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import Weatherani from '../Weather.gif';
+import React, { useState, useEffect } from 'react';
+import snoopy from '../images/snoopy.gif';
+import accat from '../images/accat.gif';
+import catfire from "../images/cat-fire.gif";
+import dumbcat from '../images/dumbcat.gif';
+
+const images = [snoopy, accat, catfire, dumbcat];
 
 const Animation = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    shuffleImages();
+  }, []);
+
+  const shuffleImages = () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setCurrentImageIndex(randomIndex);
+  };
+
   return (
     <div className='pt-5 mt-5'>
-      <img src={Weatherani} alt="Weather Animation" width="500" height="500" />
+      <img src={images[currentImageIndex]} alt="Weather Animation" width="400" height="400" />
     </div>
   );
 };
