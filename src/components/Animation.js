ef26f328dd53c8a3c54f21aaa58ece8878b83f58
sachit <sachitdahal33@gmail.com>
2024-05-08 16:00:14 +0545

    cleaner homepage

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import snoopy from '../images/snoopy.gif';
+import React, { useState } from 'react';
 import accat from '../images/accat.gif';
-import catfire from "../images/cat-fire.gif";
-import dumbcat from '../images/dumbcat.gif';
-
-const images = [snoopy, accat, catfire, dumbcat];
 
 const Animation = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
-    shuffleImages();
-  }, []);
-
-  const shuffleImages = () => {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    setCurrentImageIndex(randomIndex);
-  };
 
   return (
     <div className='pt-5 mt-5'>
-      <img src={images[currentImageIndex]} alt="Weather Animation" width="400" height="400" />
+      <img src={accat} alt="Weather Animation" width="400" height="400" />
     </div>
   );
 };
